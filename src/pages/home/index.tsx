3ec2a15201f3d9731475335c6ff16e5d3d849303
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-09 22:18:38 +0700

    feat: create and add header component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,17 @@
+import Header from "../../components/Header";
 import Home from "../../features/home";
 
 const HomePage = () => {
   return (
-    <>
+    <div className="home">
+      <img
+        className="ornament-header"
+        src="/images/ornament_header.svg"
+        alt=""
+      />
+      <Header />
       <Home />
-    </>
+    </div>
   );
 };
 
