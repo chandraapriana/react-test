0823a55592e8f707852edba89c96b8ad5531865c
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-09 22:47:57 +0700

    feat: create hero component

--- src/features/home/components/Hero.tsx
+++ src/features/home/components/Hero.tsx
@@ -1,5 +1,30 @@
+import ButtonOutline from "../../../components/Buttons/ButtonOutline";
+import ButtonSolid from "../../../components/Buttons/ButtonSolid";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <img className="img-hero" src="/images/img_hero.png" alt="" />
+      <div className="text-hero">
+        <h1>
+          Streamline Your Work, Simplify Your Life <br />
+          with Our Management App
+        </h1>
+        <p>
+          Effortless Collaboration, Efficient Result - Our Management Goods
+          <br /> Brings Your Team Toghether.
+        </p>
+        <div className="row">
+          <ButtonOutline>
+            <h1>Get Started</h1>
+          </ButtonOutline>
+          <ButtonSolid>
+            <h1>Free Trial</h1>
+          </ButtonSolid>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
