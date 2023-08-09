8a317508079a0f7bb792f4ef521aebc7885b43bf
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-09 22:18:38 +0700

    feat: create and add header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,28 @@
+import ButtonOutline from "../Buttons/ButtonOutline";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="left">
+        <img className="logo" src="/images/ic_logo.svg" alt="" />
+        <h1>Goodys</h1>
+      </div>
+      <div className="right">
+        <a className="btn-nav" href="#about">
+          About
+        </a>
+        <a className="btn-nav" href="#about">
+          Pricing
+        </a>
+        <a className="btn-nav" href="#about">
+          Contact
+        </a>
+        <ButtonOutline>
+          <p>Log in</p>
+        </ButtonOutline>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
