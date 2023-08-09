a9d56e81bd05ee293801f3ce7327253b0e5a366a
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-10 00:51:18 +0700

    feat: pricing component

--- src/features/home/components/Hero.tsx
+++ src/features/home/components/Hero.tsx
@@ -3,24 +3,26 @@ import ButtonSolid from "../../../components/Buttons/ButtonSolid";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <img className="img-hero" src="/images/img_hero.png" alt="" />
-      <div className="text-hero">
-        <h1>
-          Streamline Your Work, Simplify Your Life <br />
-          with Our Management App
-        </h1>
-        <p>
-          Effortless Collaboration, Efficient Result - Our Management Goods
-          <br /> Brings Your Team Toghether.
-        </p>
-        <div className="row">
-          <ButtonOutline>
-            <h1>Get Started</h1>
-          </ButtonOutline>
-          <ButtonSolid>
-            <h1>Free Trial</h1>
-          </ButtonSolid>
+    <div className="container-hero">
+      <div className="hero">
+        <img className="img-hero" src="/images/img_hero.png" alt="" />
+        <div className="text-hero">
+          <h1>
+            Streamline Your Work, Simplify Your Life <br />
+            with Our Management App
+          </h1>
+          <p>
+            Effortless Collaboration, Efficient Result - Our Management Goods
+            <br /> Brings Your Team Toghether.
+          </p>
+          <div className="row">
+            <ButtonOutline>
+              <h1>Get Started</h1>
+            </ButtonOutline>
+            <ButtonSolid>
+              <h1>Free Trial</h1>
+            </ButtonSolid>
+          </div>
         </div>
       </div>
       <img className="ornament-home" src="/images/ornament_home.png" alt="" />
--- src/features/home/components/Pricing.tsx
+++ src/features/home/components/Pricing.tsx
@@ -1,5 +1,97 @@
+import ButtonSolid from "../../../components/Buttons/ButtonSolid";
+
 const Pricing = () => {
-  return <div>Pricing</div>;
+  const pricePlans = [
+    {
+      tier: "Basic",
+      price: "Free",
+      desc: "Free plan for all users",
+      benefits: [
+        "Recording incoming goods",
+        "Recording outgoing goods",
+        "Recording profit results",
+      ],
+    },
+    {
+      tier: "Business",
+      price: 15,
+      desc: "Ideal for small business",
+      benefits: [
+        "Recording incoming goods",
+        "Recording outgoing goods",
+        "Recording profits",
+        "Analyzing sales results with CHART",
+        "24/7 Support",
+      ],
+    },
+    {
+      tier: "Entrepreneur",
+      price: 25,
+      desc: "Ideal for enterprise",
+      benefits: [
+        "Recording incoming goods",
+        "Recording outgoing goods",
+        "Recording profits",
+        "Analyzing sales results with CHART",
+        "24/7 Support",
+        "Exporting data to Excel",
+        "AI Income Prediction",
+      ],
+    },
+  ];
+  return (
+    <div className="pricing">
+      <h1>Pricing</h1>
+      <div className="container-price-card">
+        {pricePlans.map((plan) => (
+          <PriceCard {...plan} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const PriceCard = ({
+  tier,
+  price,
+  desc,
+  benefits,
+}: {
+  tier: string;
+  price: number | string;
+  desc: string;
+  benefits: any;
+}) => {
+  console.log(benefits);
+  return (
+    <div className="price-card">
+      <div className="content">
+        <p className="tier">{tier}</p>
+        <h1 className="price">
+          {price === "Free" ? price : `$${price}/month`}
+        </h1>
+        <p className="desc">{desc}</p>
+        <div className="container-price-benefit">
+          {benefits.map((benefit) => (
+            <PriceBenefitItem benefit={benefit} />
+          ))}
+        </div>
+      </div>
+
+      <ButtonSolid>
+        <p>Get Now</p>
+      </ButtonSolid>
+    </div>
+  );
+};
+
+const PriceBenefitItem = ({ benefit }: { benefit: string }) => {
+  return (
+    <div className="price-benefit-item">
+      <img className="bullet-point" src="/images/ellipse_pointer.svg" alt="" />
+      <p className="benefit-desc">{benefit}</p>
+    </div>
+  );
 };
 
 export default Pricing;
