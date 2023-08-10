054bcaa24623555303a688bff32f2e45b0de3591
Chandra Muhamad Apriana <aprianachandra@gmail.com>
2023-08-10 15:38:14 +0700

    feat: add authentication features

--- src/components/Inputs/Input/Input.tsx
+++ src/components/Inputs/Input/Input.tsx
@@ -1,5 +1,20 @@
-const Input = ({ placeholder }: { placeholder: string }) => {
-  return <input placeholder={placeholder} type="text" />;
+const Input = ({
+  placeholder,
+  onChange,
+  value,
+}: {
+  placeholder: string;
+  onChange: any;
+  value: string;
+}) => {
+  return (
+    <input
+      placeholder={placeholder}
+      type="text"
+      onChange={(e) => onChange(e.target.value)}
+      value={value}
+    />
+  );
 };
 
 export default Input;
---
+++ src/hooks/useHandleLogin.ts
@@ -0,0 +1,23 @@
+import { LOGGED_USER_KEY } from "../utils/constants";
+import useHandleGetUsers from "./useHandleGetUsers";
+const useHandleLogin = () => {
+  const [users] = useHandleGetUsers();
+
+  const login = (email: string, password: string) => {
+    if (!email || !password) {
+      return { status: false, message: "Input is required" };
+    }
+    const isFoundUser = users?.find(
+      (user) => user.email === email && user.password === password
+    );
+    if (!isFoundUser) {
+      return { status: false, message: "User Not Found" };
+    }
+    window.localStorage.setItem(LOGGED_USER_KEY, JSON.stringify(isFoundUser));
+    return { status: true, message: "Login Successfull" };
+  };
+
+  return [login];
+};
+
+export default useHandleLogin;
